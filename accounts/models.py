c57eefa1e2530b534a3a53f603fecd259b896b34
shadon016 <shadon016@gmail.com>
2023-08-14 11:38:03 +0600

    add profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+state_choices=(
+    ('Dhaka','Dhaka'),
+    ('Chittagong','Chittagong'),
+('Sylhet','Sylhet'),
+    ('Khulna','Khulna'),
+('Rajshahi','Rajshahi'),
+    ('Rangpur','Rangpur'),
+
+)
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
+    bio=models.CharField(max_length=50)
+    photo=models.ImageField(upload_to='profile')
+    state=models.CharField(max_length=30,choices=state_choices)
+    city=models.CharField(max_length=20)
+    town=models.CharField(max_length=20)
+    zip_code=models.CharField(max_length=10)
+    phone=models.IntegerField(max_length=15)
+    updated_at=models.DateTimeField(auto_now=True)
