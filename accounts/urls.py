be1ba3faa3c048f49bb2b727caa7c5a305c38287
shadon016 <shadon016@gmail.com>
2023-08-16 00:04:09 +0600

    working profile features

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,9 @@
-
 from django.urls import path
 from .views import *
 
 urlpatterns = [
-
-    path('', Register, name='signup'),
-    path('login', loginView, name='login'),
-    path('logout',logoutView,name='logout'),
-
+    path("", Register, name="signup"),
+    path("login", loginView, name="login"),
+    path("logout", logoutView, name="logout"),
+    path("profile", profile, name="profile"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,34 +1,34 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 
 
 def Register(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        pass1 = request.POST.get('password1')
-        pass2 = request.POST.get('password2')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        pass1 = request.POST.get("password1")
+        pass2 = request.POST.get("password2")
 
         if pass1 != pass2:
             return HttpResponse("Your password and confrom password are not Same!!")
         else:
-
             user = User.objects.create_user(username, email, pass1)
             user.save()
-            return redirect('/')
+            return redirect("/")
 
-    return render(request, 'accounts/signup.html')
+    return render(request, "accounts/signup.html")
+
 
 def loginView(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('pass')
+        username = request.POST.get("username")
+        password = request.POST.get("pass")
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request,user)
-            return redirect('/')
+            login(request, user)
+            return redirect("/")
         else:
             return HttpResponse("Username or Password is incorrect!!!")
 
@@ -37,4 +37,8 @@ def loginView(request):
 
 def logoutView(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
+
+
+def profile(request):
+    return render(request, "accounts/profile.html")
